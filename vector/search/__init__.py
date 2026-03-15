b5e448ffe970cf7dc397012bf890c3d591576eb0
Ravikumar Shah <ravikumar.shah.2804@gmail.com>
2026-03-15 14:10:13 +0900

    [search] export full public API from search module

--- vector/search/__init__.py
+++ vector/search/__init__.py
@@ -1,6 +1,13 @@
 """Optuna NSGA-II search engine for ESN hyperparameter optimization."""
 
 from vector.search.config import load_search_config
+from vector.search.engine import create_or_load_study, run_search
 from vector.search.objective import objective, sample_sequences
 
-__all__ = ["load_search_config", "objective", "sample_sequences"]
+__all__ = [
+    "load_search_config",
+    "create_or_load_study",
+    "run_search",
+    "objective",
+    "sample_sequences",
+]
